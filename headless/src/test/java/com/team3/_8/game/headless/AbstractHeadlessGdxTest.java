f2d8c83031e7f0cf563e17ee0c23bad606873066
Daniel Thwaites <zfk509@york.ac.uk>
2025-11-30 20:32:00 +0000

    test: clear Bob between tests

--- headless/src/test/java/com/team3/_8/game/headless/AbstractHeadlessGdxTest.java
+++ headless/src/test/java/com/team3/_8/game/headless/AbstractHeadlessGdxTest.java
@@ -2,6 +2,7 @@ package com.team3._8.game.headless;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.team3._8.game.entities.Bob;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.mockito.Mockito.mock;
@@ -11,5 +12,7 @@ public abstract class AbstractHeadlessGdxTest {
     public void setup() {
         Gdx.gl = Gdx.gl20 = mock(GL20.class);
         HeadlessLauncher.main(new String[0]);
+
+        Bob.bob = null;
     }
 }
