bd43a2ae5512b65e4c13c9e69142966a983005b0
Daniel Thwaites <zfk509@york.ac.uk>
2025-12-08 23:31:04 +0000

    test: test `GameScreen.resize`
    Removed the constructor test because we need to construct the screen in this test and any future tests anyway.

--- headless/src/test/java/com/team3/_8/game/headless/screens/GameScreenTests.java
+++ headless/src/test/java/com/team3/_8/game/headless/screens/GameScreenTests.java
@@ -6,9 +6,18 @@ import com.team3._8.game.headless.AbstractHeadlessGdxTest;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameScreenTests extends AbstractHeadlessGdxTest {
     @Test
-    public void constructorWorks() {
-        new GameScreen(new MazeGame());
+    public void resizeChangesViewportSize() {
+        MazeGame game = new MazeGame();
+        game.create();
+        GameScreen screen = new GameScreen(game);
+
+        screen.resize(912, 403);
+
+        assertEquals(912, game.viewport.getScreenWidth());
+        assertEquals(403, game.viewport.getScreenHeight());
     }
 }
